ac9b844330a0c591c38099bbb724d2092eb40ac0
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-12-10 14:35:52 -0600

    added calls for all movies, single movie, and create movie; need to add functionality to html

--- movie-app.js
+++ movie-app.js
@@ -3,7 +3,10 @@
 
 
 $(document).ready(function () {
-    const serverURL = 'sordid-psychedelic-nylon';
+//  url for API
+    const API_URL = 'https://sordid-psychedelic-nylon.glitch.me/movies'
+//  we should be able to delete this
+//     const serverURL = 'sordid-psychedelic-nylon';
 
 
 //fadeout loading message, removing div to give space for movies
@@ -15,6 +18,60 @@ $(document).ready(function () {
     });
 
 
+//  getting all movies here
+    let getAllMovies = () => {
+        return fetch(API_URL)
+            .then(resp => resp.json())
+            .then(data => {
+                // console.log(data);
+                for (let property of data) {
+                    console.log(`Movie List:\nTitle: ${property.title}\nActors:${property.actors}\nYear: ${property.year}`)
+                }
+            })
+            .catch(err => console.error("This is your err:", err));
+    }
+    getAllMovies();
+
+// GET REQUEST BY ID
+    let getMovieById = (id) => {
+        return fetch(`${API_URL}/${id}`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log("This is your single movie data: ", data);
+                // console.log(`Console Logging\nTitle: ${data.title} \nDirector: ${data.director} \nActor(s): ${data.actors} \nYear: ${data.year}`);
+                return `\nTitle: ${data.title} \nDirector: ${data.director} \nActor(s): ${data.actors} \nYear: ${data.year}`;
+            })
+            .catch(err => console.error("This is your err:", err))
+    }
+
+    getMovieById(2).then(data => console.log("This is your selected movie: ", data));
+
+//  new object variable
+//  will use once we start working on creating/adding new movies
+    let newMovie = {
+        id: 100,
+        actors: "",
+        director: "",
+        genre: "",
+        plot: "",
+        poster: "",
+        rating: "",
+        title: "",
+        year: ""
+    }
+
+//  variable to create movie below
+//  will incorporate user input into the request
+    let createMovie = () => {
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify()
+        }
+        return fetch(API_URL, options).then(resp => resp.json()).catch(err => console.error("This is your err: ", err));
+    }
 
 
 
